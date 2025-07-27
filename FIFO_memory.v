db3e7f340a7f0430b4a44903b0d6ae923332c7c5
Sairaghava <164220290+sairaghava2542@users.noreply.github.com>
2025-07-27 15:33:53 +0530

    Update FIFO_memory.v

--- FIFO_memory.v
+++ FIFO_memory.v
@@ -1,15 +1,3 @@
-//----------------DISCRIPTION-----------------
-// This code is for FIFO memory module. It is 
-// a synchronous FIFO memory module with a
-// configurable data size and address size.
-// It is a dual-port memory module.
-//-------------------------------------------
-
-// -------------PARAMETERS------------
-// DATA_SIZE: Size of the data bus
-// ADDR_SIZE: Size of the address bus
-// -----------------------------------
-
 module FIFO_memory #(parameter DATA_SIZE = 8,
     parameter ADDR_SIZE = 4)(
     output [DATA_SIZE-1:0] rdata,        // Output data - data to be read
